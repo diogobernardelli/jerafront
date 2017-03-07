5837276d7d1154e68b1b58a798aa1b203fda4b1f
DIOGO <diogo@diogobernardelli.com.br>
2017-03-07 10:24:17 -0400

    Separando os arquivos index e header / correçoes fontes Google

--- index.php
+++ index.php
@@ -1,39 +1,8 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta name="Author" CONTENT=""/>
-		<meta name="Custodian" content="" />
-		<meta name="DC.Identifier" content=""/> 
-		<meta name="copyright" content="© 2017 - JERA" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<meta http-equiv="Content-Language" content="pt-br"/>
-		<meta http-equiv="cache-control"   content="no-cache" />
-		<meta http-equiv="pragma" content="no-cache" />
-		<meta name="revisit-after" content="5 days" />
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		
-		<title>JERA</title>
-		
-		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-        <link rel="shortcut icon" type="image/ico" href="favicon.ico">
-		
-		<link href="css/reset.css" rel="stylesheet" type="text/css" /> 
-		<link href="css/style.css?v1.0" rel="stylesheet" type="text/css" />  
-		<link href="css/font-awesome.css" rel="stylesheet" type="text/css" />
-        <link href="css/roboto-fontface.css" rel="stylesheet" >
-        <link href="css/roboto-condensed-fontface.css" rel="stylesheet">
-	</head>
-    
-    <!-- SCRIPTS -->
-    <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
-    <script type="text/javascript" src="js/scripts.js"></script>
+<?php include "header.php" ?>
     
 <body>
     <header>
-        <div class="hcontent">
+        <div class="hcontent size">
             <img srcset="images/logo-jera-header.png 1x,
                          images/logo-jera-header@1.5x.png 1.5x,
                          images/logo-jera-header@2x.png 2x,
@@ -51,12 +20,14 @@
                 </ul>
             </nav>
         </div>
-        <div class="call">
+        <div class="call size">
             <img srcset="images/logo_jera-labs.png 1x,
                          images/logo_jera-labs@2x.png 2x">
             <h3>Onde as idéias ganham forma</h3>
-            <h4>Labs é o nosso repositório de ideias da nossa equipe que foram tiradas do papel e viraram projetos internos mais divertidos.</h4>
+            <p>Labs é o nosso repositório de ideias da nossa equipe que foram tiradas do papel e viraram projetos internos mais divertidos.</p>
         </div>
     </header>
+    
+    
 </body>
 </html>
