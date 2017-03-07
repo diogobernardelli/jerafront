49287c482d9d33ffb0384cbdb93cba637b62d292
DIOGO <diogo@diogobernardelli.com.br>
2017-03-07 10:09:39 -0400

    Deletando fonte desnecessárias / Alteraçoes inicias Header LandingPage

--- index.php
+++ index.php
@@ -32,6 +32,31 @@
     <script type="text/javascript" src="js/scripts.js"></script>
     
 <body>
-    teste
+    <header>
+        <div class="hcontent">
+            <img srcset="images/logo-jera-header.png 1x,
+                         images/logo-jera-header@1.5x.png 1.5x,
+                         images/logo-jera-header@2x.png 2x,
+                         images/logo-jera-header@3x.png 3x">
+            
+            <nav>
+                <ul>
+                    <li><a href="javascript:;">quem somos</a></li>
+                    <li><a href="javascript:;">labs</a></li>
+                    <li><a href="javascript:;">o que fazemos</a></li>
+                    <li><a href="javascript:;">startups</a></li>
+                    <li><a href="javascript:;">empresas</a></li>
+                    <li><a href="javascript:;">blog</a></li>
+                    <li><a href="javascript:;">contato</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="call">
+            <img srcset="images/logo_jera-labs.png 1x,
+                         images/logo_jera-labs@2x.png 2x">
+            <h3>Onde as idéias ganham forma</h3>
+            <h4>Labs é o nosso repositório de ideias da nossa equipe que foram tiradas do papel e viraram projetos internos mais divertidos.</h4>
+        </div>
+    </header>
 </body>
 </html>
